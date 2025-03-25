a478e98a5e6ed601ed99af6e296040eb613ca07f
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-03-25 08:43:36 -0400

    Tree

--- verdantvibe/src/App.js
+++ verdantvibe/src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <h1>Hello World</h1>
+    <h2>ujhfsjsfdj</h2>
+    </>
   );
 }
 
---
+++ verdantvibe/src/components/Tree.jsx
@@ -0,0 +1,11 @@
+import "./css/Tree.css"
+
+function Tree() {
+    return(
+        <h1> I am a tree </h1>
+    );
+
+    
+}
+
+export default Tree;
