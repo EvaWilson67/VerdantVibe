2c664f4c401f00263866b6e9b5404d7ab34053d3
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-03-27 09:11:56 -0400

    website

--- verdantvibe/src/pages/Home.jsx
+++ verdantvibe/src/pages/Home.jsx
@@ -1,24 +1,25 @@
 import "./css/Home.css";
-import Tree from "../components/Tree";
+// import Tree from "../components/Tree";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
-    <>
-      <section className="columns">
-        <Tree
-          name="Live Oak"
-          Description="doesn't lose it's leave"
-          image="images/oak.jpg"
-        />
-        <Tree
-          name="Dogwood"
-          Description="flowers in spring"
-          image="images/oak.jpg"
-        />
-      </section>
-    </>
+    <h1>hello</h1>
+    // <>
+    //   <section className="columns">
+    //     <Tree
+    //       name="Live Oak"
+    //       Description="doesn't lose it's leave"
+    //       image="images/oak.jpg"
+    //     />
+    //     <Tree
+    //       name="Dogwood"
+    //       Description="flowers in spring"
+    //       image="images/oak.jpg"
+    //     />
+    //   </section>
+    // </>
   );
 }
 
