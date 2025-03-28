240b502f2c0a9965c08b03f325c8f3d5f929a62b
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2025-03-27 21:21:39 -0400

    website

--- verdantvibe/src/index.js
+++ verdantvibe/src/index.js
@@ -1,29 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import Layout from "./Layout";
-import Home from "./pages/Home";
-import Blog from "./pages/Blog";
-import About from "./pages/About";
-import Nopage from "./pages/Nopage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './index.css';
+import Layout from './Layout';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import About from './pages/About';
+import Nopage from './pages/Nopage';
 
 function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="*" element={<Nopage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  return(
+    <h1>FUCK</h1>
+    // <BrowserRouter basename={process.env.PUBLIC_URL}>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="about" element={<About />} />
+    //       <Route path="blog" element={<Blog />} />
+    //       <Route path="*" element={<Nopage/>} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
   );
-}
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
